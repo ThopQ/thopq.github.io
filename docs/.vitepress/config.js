0cd78cd4456735d9ffb8433cf5f6449b5e85bceb
Thomas Iseli <thomas.iseli@webyourself.ch>
2023-01-23 23:10:09 +0100

    config with sidebar

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,17 @@
+export default {
+  title: "Knowledgebase",
+  description: "For personal documentation purpose...",
+
+  themeConfig: {
+    nav: [{ text: "GitHub", link: "https://github.com/ThopQ/thopq.github.io" }],
+    sidebar: [
+      {
+        text: "Guide",
+        items: [
+          { text: "Introduction", link: "/" },
+          { text: "Getting Started", link: "/about" },
+        ],
+      },
+    ],
+  },
+};
